fc4374a97d16365a90911aeacea0e45d43da0e14
landofcoder <landofcoder@gmail.com>
2020-11-12 11:16:38 +0700

    updated files

--- Controller/Standard/Response.php
+++ Controller/Standard/Response.php
@@ -13,7 +13,8 @@ class Response extends \Lof\Cfcheckout\Controller\CfAbstract {
             $status = $paymentMethod->validateResponse($params);
             $order = $this->getOrder();
             $orderStatus = $order->getStatus();
-            if($orderStatus=="pending"){ 
+            
+            if($orderStatus=="pending"){
                 if ($status == "SUCCESS") {
                     $returnUrl = $this->getCheckoutHelper()->getUrl('checkout/onepage/success');
                     $payment = $order->getPayment();
@@ -21,25 +22,63 @@ class Response extends \Lof\Cfcheckout\Controller\CfAbstract {
                     $this->messageManager->addSuccess(__('Your payment was successful'));
 
                 } else if ($status == "CANCELLED") {
-                     $order->cancel()->save();
+                    $this->_eventManager->dispatch(
+                        'lof_cfcheckout_controller_standard_response',
+                        [
+                            'order_ids' => [$order->getLastOrderId()],
+                            'order' => $order,
+                            'status' => $status
+                        ]
+                    );
+                    $order->cancel()->save();
                     //$this->messageManager->addErrorMessage(__('Your payment has been cancelled'));
                     $this->_cancelPayment();
                     
                 } else if ($status == "FAILED") {
+                    $this->_eventManager->dispatch(
+                        'lof_cfcheckout_controller_standard_response',
+                        [
+                            'order_ids' => [$order->getLastOrderId()],
+                            'order' => $order,
+                            'status' => $status
+                        ]
+                    );
                     $order->cancel()->save();
                     $this->_checkoutSession->restoreQuote();
                     $this->messageManager->addErrorMessage(__('Payment failed. Please try again or choose a different payment method'));
                     $returnUrl = $this->getCheckoutHelper()->getUrl('checkout/onepage/failure');
                 } else if($status == "PENDING"){
-                    
+                    $this->_eventManager->dispatch(
+                        'lof_cfcheckout_controller_standard_response',
+                        [
+                            'order_ids' => [$order->getLastOrderId()],
+                            'order' => $order,
+                            'status' => $status
+                        ]
+                    );
                     $this->messageManager->addWarning(__('Your payment is pending'));
 
                 } else{
+                    $this->_eventManager->dispatch(
+                        'lof_cfcheckout_controller_standard_response',
+                        [
+                            'order_ids' => [$order->getLastOrderId()],
+                            'order' => $order,
+                            'status' => $status
+                        ]
+                    );
                     $this->messageManager->addErrorMessage(__('There is an error.Payment status is pending'));
                     $returnUrl = $this->getCheckoutHelper()->getUrl('checkout/onepage/failure');
                 }
             } else {
-
+                $this->_eventManager->dispatch(
+                    'lof_cfcheckout_controller_standard_response',
+                    [
+                        'order_ids' => [$order->getLastOrderId()],
+                        'order' => $order,
+                        'status' => $status
+                    ]
+                );
                 $this->messageManager->addNotice(__('Your payment was already processed'));
             }    
         } catch (\Magento\Framework\Exception\LocalizedException $e) {
